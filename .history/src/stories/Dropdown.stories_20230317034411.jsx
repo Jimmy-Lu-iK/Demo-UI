b3f98e89292b6e007de311b4b08b500357014ff6
Jimmy-Lu-iK <jinmingcharlielu@gmail.com>
2023-03-17 05:10:06 -0700

    upgrade source code display

---
+++ .history/src/stories/Dropdown.stories_20230317034411.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import Dropdown from '../components/Dropdown'
+import DropdownMDX from './docs/DropdownMDX.mdx'
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Components/Dropdown',
+  component: Dropdown,
+  parameters: {
+    docs: {
+      page: DropdownMDX,
+    },
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+
+export const Instance = () => {
+  const options = ['Apple', 'Banana', 'Peach', 'Kiwi', 'Orange']
+
+  return (
+    <Dropdown options={options} />
+  )
+};
+Instance.parameters = {
+  docs: {
+    source: {
+      code: '<h1>Hello world</h1>',
+      language: "yml",
+      type: "auto",
+    },
+  },
+};
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
